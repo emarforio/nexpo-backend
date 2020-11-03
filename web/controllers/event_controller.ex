1f2ec33cf1aece4bb1c96f39c8291ec96eab9d64
Anthony Bui <an7407bu-s@student.lu.se>
2020-11-03 17:55:06 +0100

    apidoc for event (NOT generated yet)

--- web/controllers/event_controller.ex
+++ web/controllers/event_controller.ex
@@ -17,12 +17,70 @@ defmodule Nexpo.EventController do
     when action in [:get_all_tickets]
   )
 
+  @apidoc """
+  @api {GET} /api/events Get all events
+  @apiGroup Events
+  @apiDescription Fetch all available events
+  @apiSuccessExample {json} Success
+
+  HTTP 200 OK
+  {
+    "data": [
+      {
+        "start": "15:10",
+        "name": "Bounce",
+        "location": "Outside Kårhuset, bus to Malmö",
+        "id": 1,
+        "end": "17:30",
+        "date": "Nov 3rd - Sunday"
+      },
+      {
+        "start": "17:15",
+        "name": "The digital shift - how will you be affected?",
+        "location": "Kårhuset: Auditorium",
+        "id": 2,
+        "end": "18:45",
+        "date": "Nov 4rd - Monday"
+      }
+    ]
+  }
+
+  """
   def index(conn, %{}, _user, _claims) do
     events = Repo.all(Event)
 
     render(conn, "index.json", events: events)
   end
 
+  @apidoc """
+  @api {GET} /api/event/:id Get an event
+  @apiGroup Events
+  @apiDescription Fetch a single event
+  @apiSuccessExample {json} Success
+
+  HTTP 200 OK
+  {
+    "data": {
+      "tickets_left": 19,
+      "start": "15:10",
+      "name": "Bounce",
+      "location": "Outside Kårhuset, bus to Malmö",
+      "id": 1,
+      "event_info": {
+        "tickets": 20,
+        "language": null,
+        "id": 1,
+        "host": null,
+        "description": "Placeholder"
+      },
+      "end": "17:30",
+      "date": "Nov 3rd - Sunday"
+    }
+  }
+
+  @apiUse NotFoundError
+  @apiUse BadRequestError
+  """
   def get_event(conn, %{"id" => event_id}, _user, _claims) do
     event =
       Repo.get!(Event, event_id)
@@ -47,6 +105,27 @@ defmodule Nexpo.EventController do
     end
   end
 
+  @apidoc """
+  @api {GET} /api/event/tickets Get tickets
+  @apiGroup Events
+  @apiDescription Fetch all tickets for this student
+  @apiSuccessExample {json} Success
+
+  HTTP 200 OK
+  {
+    "data": [
+      {
+        "ticket_code": "$2b$04$WbQloXKuTH02YJ4bqzuPCOcFakIKwXJI74Wce.jJsBq7RJHqsn6g.",
+        "photo": null,
+        "id": 1,
+        "event_id": 1
+      }
+    ]
+  }
+
+  @apiUse BadRequestError
+  @apiUse UnauthorizedError
+  """
   def get_tickets(conn, %{}, user, _claims) do
     user_student =
       user
@@ -69,12 +148,61 @@ defmodule Nexpo.EventController do
     end
   end
 
+  @apidoc """
+  @api {GET} /api/admin/event/tickets Get all event tickets
+  @apiGroup Events
+  @apiDescription Fetch all event tickets as admin
+  @apiSuccessExample {json} Success
+
+  HTTP 200 OK
+  {
+    "data": [
+      {
+        "ticket_code": "$2b$04$ma5/mqWtDmFJqF2jds3.o.esB5UNBYv7t4ZPwwkbHe5tABYdhWm0C",
+        "photo": true,
+        "id": 35,
+        "event_id": 1
+      },
+      {
+        "ticket_code": "$2b$04$8fvB3uOgGfxWyBkihEJyW.hsN6YtJJ5ohxc28dg3B4QOuLSelTpLm",
+        "photo": false,
+        "id": 36,
+        "event_id": 2
+      }
+    ]
+  }
+
+  @apiUse UnauthorizedError
+  """
   def get_all_tickets(conn, %{}, _user, _claims) do
     tickets = Repo.all(EventTicket)
 
     render(conn, Nexpo.EventTicketView, "index.json", event_tickets: tickets)
   end
 
+  @apidoc """
+  @api {PUT} /api/event/ticket Create ticket
+  @apiGroup Events
+  @apiDescription Create a ticket for this student
+  @apiSuccessExample {json} Success
+
+  HTTP 201 Created
+  {
+    "data": [
+      {
+        "ticket_code": "$2b$04$WbQloXKuTH02YJ4bqzuPCOcFakIKwXJI74Wce.jJsBq7RJHqsn6g.",
+        "photo": true,
+        "id": 1,
+        "event_id": 1
+      }
+    ]
+  }
+
+  @apiUse BadRequestError
+  @apiUse UnauthorizedError
+  @apiUse UnprocessableEntity
+  @apiUse NotFoundError
+  """
   def create_ticket(conn, %{"event_id" => event_id, "photo" => photo}, user, _claims) do
     user_student =
       user
@@ -118,6 +246,19 @@ defmodule Nexpo.EventController do
     end
   end
 
+  @apidoc """
+  @api {DELETE} /api/event/ticket/:id Delete a ticket
+  @apiGroup Events
+  @apiDescription Remove a ticket for this student
+  @apiSuccessExample {json} Success
+
+  HTTP 204 OK
+
+  @apiUse BadRequestError
+  @apiUse UnauthorizedError
+  @apiUse NotFoundError
+  @apiUse UnprocessableEntity
+  """
   def remove_ticket(conn, %{"id" => event_id}, user, _claims) do
     user_student =
       user
