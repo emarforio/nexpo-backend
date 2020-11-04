a53b61bfd206dbb715ae13b6353425d860542a50
Anthony Bui <an7407bu-s@student.lu.se>
2020-11-05 00:40:16 +0100

    Generated docs for Event controller

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,15 +1,1 @@
-define({
-  "name": "Nexpo Backend",
-  "version": "1.0.0",
-  "description": "An internal system for ARKAD",
-  "title": "Nexpo Backend - Documentation",
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2020-11-04T17:23:13.871Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.7"
-  }
-});
+define({  "name": "Nexpo Backend",  "version": "1.0.0",  "description": "An internal system for ARKAD",  "title": "Nexpo Backend - Documentation",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2020-11-04T23:38:53.153Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
